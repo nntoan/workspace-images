28ea24282acc25edb4289292e7d1b52ef5a6fc2d
Toan Nguyen <nntoan@users.noreply.github.com>
2024-06-25 21:32:10 +0700

    Copy docker-entrypoint.d scripts into images

--- images/php/cli/data/docker-entrypoint.sh
+++ images/php/cli/data/docker-entrypoint.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
 
-[ "$DEBUG" = "true" ] && set -x
+set -e
+set -u
+set -o pipefail
 
+
+###
+### Globals
+###
+
+# The following global variables are available by our Dockerfile itself:
+#   MY_USER
+#   MY_GROUP
+#   MY_UID
+#   MY_GID
+
+# Path to scripts to source
+CONFIG_DIR="/docker-entrypoint.d"
+
+
+###
+### Source libs
+###
+init="$( find "${CONFIG_DIR}" -name '*.sh' -type f | sort -u )"
+for f in ${init}; do
+	# shellcheck disable=SC1090
+	. "${f}"
+done
+
+#############################################################
+## Entry Point
+#############################################################
+
+###
+### Set Debug level
+###
+[ "$DEBUG" = "true" ] && set -x
+DEBUG_LEVEL="$( env_get "DEBUG_ENTRYPOINT" "0" )"
+log "info" "Debug level: ${DEBUG_LEVEL}" "${DEBUG_LEVEL}"
+
+
+###
+### Change uid/gid
+###
+set_uid "NEW_UID" "${MY_USER}" "${DOCROOT}" "${DEBUG_LEVEL}"
+set_gid "NEW_GID" "${MY_GROUP}" "${DOCROOT}" "${DEBUG_LEVEL}"
+
+
+###
+### Setup crontab
+###
 if [ ! -z "${CRONTAB}" ]; then
     echo "${CRONTAB}" > /etc/cron.d/magento && touch /var/log/cron.log
 fi
 
-PHP_EXT_DIR=/usr/local/etc/php/conf.d
 
-# Enable PHP extensions
+###
+### Enable PHP extensions
+###
+PHP_EXT_DIR=/usr/local/etc/php/conf.d
 PHP_EXT_COM_ON=docker-php-ext-enable
 
 [ -d ${PHP_EXT_DIR} ] && rm -f ${PHP_EXT_DIR}/docker-php-ext-*.ini
@@ -17,11 +67,17 @@ if [ -x "$(command -v ${PHP_EXT_COM_ON})" ] && [ ! -z "${PHP_EXTENSIONS}" ]; the
   ${PHP_EXT_COM_ON} ${PHP_EXTENSIONS}
 fi
 
-# Clear composer cache if needed
+
+###
+### Clear composer cache if needed
+###
 [ "$COMPOSER_CLEAR_CACHE" = "true" ] && \
     composer clearcache
 
-# Configure composer
+
+###
+### Configure composer
+###
 [ ! -z "${COMPOSER_VERSION}" ] && \
     composer self-update $COMPOSER_VERSION
 
@@ -32,4 +88,8 @@ fi
     composer config --global http-basic.repo.magento.com \
         $COMPOSER_MAGENTO_USERNAME $COMPOSER_MAGENTO_PASSWORD
 
+
+###
+### Startup
+###
 exec "$@"
--- images/php/fpm/data/docker-entrypoint.sh
+++ images/php/fpm/data/docker-entrypoint.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
+set -e
+set -u
+set -o pipefail
+
+
+###
+### Globals
+###
+
+# The following global variables are available by our Dockerfile itself:
+#   MY_USER
+#   MY_GROUP
+#   MY_UID
+#   MY_GID
+
+# Path to scripts to source
+CONFIG_DIR="/docker-entrypoint.d"
+
+
+###
+### Source libs
+###
+init="$( find "${CONFIG_DIR}" -name '*.sh' -type f | sort -u )"
+for f in ${init}; do
+	# shellcheck disable=SC1090
+	. "${f}"
+done
+
+#############################################################
+## Entry Point
+#############################################################
+
+###
+### Set Debug level
+###
 [ "$DEBUG" = "true" ] && set -x
+DEBUG_LEVEL="$( env_get "DEBUG_ENTRYPOINT" "0" )"
+log "info" "Debug level: ${DEBUG_LEVEL}" "${DEBUG_LEVEL}"
 
+
+###
+### Change uid/gid
+###
+set_uid "NEW_UID" "${MY_USER}" "${DOCROOT}" "${DEBUG_LEVEL}"
+set_gid "NEW_GID" "${MY_GROUP}" "${DOCROOT}" "${DEBUG_LEVEL}"
+
+
+###
+### Enable PHP extensions
+###
 PHP_EXT_DIR=/usr/local/etc/php/conf.d
-
-# Enable PHP extensions
 PHP_EXT_COM_ON=docker-php-ext-enable
 
 [ -d ${PHP_EXT_DIR} ] && rm -f ${PHP_EXT_DIR}/docker-php-ext-*.ini
@@ -13,7 +59,10 @@ if [ -x "$(command -v ${PHP_EXT_COM_ON})" ] && [ ! -z "${PHP_EXTENSIONS}" ]; the
   ${PHP_EXT_COM_ON} "${PHP_EXTENSIONS}"
 fi
 
-# Set host.docker.internal if not available
+
+###
+### Set host.docker.internal if not available
+###
 HOST_NAME="host.docker.internal"
 HOST_IP=$(php -r "putenv('RES_OPTIONS=retrans:1 retry:1 timeout:1 attempts:1'); echo gethostbyname('$HOST_NAME');")
 if [[ "$HOST_IP" == "$HOST_NAME" ]]; then
@@ -21,4 +70,9 @@ if [[ "$HOST_IP" == "$HOST_NAME" ]]; then
   printf "\n%s %s\n" "$HOST_IP" "$HOST_NAME" >> /etc/hosts
 fi
 
+
+###
+### Startup
+###
+log "info" "Starting $( php-fpm -v 2>&1 | head -1 )" "${DEBUG_LEVEL}"
 exec "$@"
