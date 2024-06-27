9b45ad12c45ace9039945e815f7864836339fdbf
Toan Nguyen <nntoan@users.noreply.github.com>
2024-06-27 16:37:07 +0700

    Added MailHog generator

---
+++ images/mailhog/data/docker-entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+
+###
+### Globals
+###
+
+# The following global variables are available by our Dockerfile itself:
+#   MY_USER
+#   MY_GROUP
+#   MY_UID
+#   MY_GID
+
+# Path to scripts to source
+CONFIG_DIR="/docker-entrypoint.d"
+
+
+###
+### Source libs
+###
+init="$( find "${CONFIG_DIR}" -name '*.sh' -type f | sort -u )"
+for f in ${init}; do
+	# shellcheck disable=SC1090
+	. "${f}"
+done
+
+#############################################################
+## Entry Point
+#############################################################
+
+###
+### Set Debug level
+###
+DEBUG_LEVEL="$( env_get "DEBUG_ENTRYPOINT" "0" )"
+log "info" "Debug level: ${DEBUG_LEVEL}" "${DEBUG_LEVEL}"
+
+
+###
+### Change uid/gid
+###
+set_uid "${NEW_UID}" "${MY_USER}"
+set_gid "${NEW_GID}" "${MY_USER}" "${MY_GROUP}"
+
+
+###
+### Startup
+###
+exec "$@"
